4a4be6d171001b3ccfed39a08c49568a1681b7d6
ChristianSantamaria <christiancameselle@hotmail.com>
2021-07-26 10:41:09 +0200

    [ADD] purchase_order_type_advanced

---
+++ purchase_order_type_advanced/__manifest__.py
@@ -0,0 +1,23 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Purchase Order Type Advanced",
+    "summary": """
+        Adds extra fields to the purchase order type (journal, picking type,...)
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    'category': "Operations/Purchase",
+    "website": "https://github.com/solvosci/slv-purchase",
+    "depends": [
+        "purchase_stock",
+        "purchase_order_type"
+    ],
+    "data": [
+        "views/purchase_order_type_views.xml",
+        "views/purchase_order_views.xml",
+        "views/account_move_views.xml"
+    ],
+    'installable': True,
+}
---
+++ purchase_order_type_advanced/models/purchase_order.py
@@ -0,0 +1,30 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+
+from odoo import api, fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    pricelist_id = fields.Many2one(
+        comodel_name="product.pricelist",
+        strint="Pricelist"
+    )
+
+    @api.onchange("order_type")
+    def onchange_order_type(self):
+        super().onchange_order_type()
+        for order in self:
+            if order.order_type.picking_type_id:
+                order.picking_type_id = order.order_type.picking_type_id
+            if order.order_type.pricelist_id:
+                order.pricelist_id = order.order_type.pricelist_id
+
+    def action_view_invoice(self):
+        res = super(PurchaseOrder, self).action_view_invoice()
+        if self.order_type.journal_id:
+            res["default_journal_id"] = self.order_type.journal_id.id
+        if self.order_type:
+            res["default_purchase_type_id"] = self.order_type.id
+        return res
