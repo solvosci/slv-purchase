0176740053c266528d0e37e190ffa2a2187a1715
David Alonso // Solvos <david.alonso@solvos.es>
2022-11-14 13:50:08 +0100

    [FIX] purchase_order_type_advanced: bug in default purchase order type domain for input picking types

--- purchase_order_type_advanced/__manifest__.py
+++ purchase_order_type_advanced/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     'category': "Operations/Purchase",
     "website": "https://github.com/solvosci/slv-purchase",
     "depends": [
