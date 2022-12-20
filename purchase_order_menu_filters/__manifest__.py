3bd3cf2725768565454be75d51d21a3187168364
LuPinheiro <gerallupinheiro@gmail.com>
2022-12-20 12:42:10 +0100

    [ADD] purchase_order_menu_filters

---
+++ purchase_order_menu_filters/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Purchase Order Menu Filters",
+    "summary": """
+        Adds filters in purchase order
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Inventory/Purchase",
+    "website": "https://github.com/solvosci/slv-purchase",
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+        "views/purchase_order_views.xml",
+    ],
+    'installable': True,
+}
