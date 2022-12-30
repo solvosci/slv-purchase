e5bb47b27c2f92c7a5f0bb55598992df69d61b46
David Alonso // Solvos <david.alonso@solvos.es>
2022-12-30 18:31:37 +0100

    [ADD] purchase_order_decrease_qty

---
+++ purchase_order_decrease_qty/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Purchase Order Decrease Quantity",
+    "summary": """
+        Enables purchase line quantity decrease even if quantity received
+        is greater than the new line quantity
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "14.0.1.0.0",
+    "category": "Inventory/Purchase",
+    "website": "https://github.com/solvosci/slv-purchase",
+    "depends": ["purchase_stock"],
+    'installable': True,
+}
---
+++ purchase_order_decrease_qty/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+Enables purchase line quantity decrease even if quantity received is greater
+than the new line quantity.
