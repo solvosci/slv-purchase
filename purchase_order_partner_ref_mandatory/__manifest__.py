742e084e6330bcdcfeff4954151fdf4a1ffda562
LuPinheiro <gerallupinheiro@gmail.com>
2023-01-31 13:49:16 +0100

    [IMP] purchase_order_partner_ref_mandatory: improvement

--- purchase_order_partner_ref_mandatory/__manifest__.py
+++ purchase_order_partner_ref_mandatory/__manifest__.py
@@ -1,14 +1,14 @@
 # © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
 # License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
 {
-    "name": "Purchase Order Partner Ref Mandatory",
+    "name": "Highlight Partner Ref field in some views",
     "summary": """
-        Adds Partner Ref Mandatory in purchase order
+        Highlight Partner Ref field in some views
     """,
     "author": "Solvos",
     "license": "LGPL-3",
     "version": "15.0.1.1.0",
-    "category": "Purchase",
+    "category": "Inventory/Purchase",
     "website": "https://github.com/solvosci/slv-purchase",
     "depends": [
         "purchase",
