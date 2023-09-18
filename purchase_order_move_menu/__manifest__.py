2412040828d20aec44b824e57dc84a384c5ac19d
Christian Santamaria <christian.santamaria@solvos.es>
2023-09-18 10:25:55 +0200

    [MIG] purchase_order_move_menu: Migration to 14.0

--- purchase_order_move_menu/__manifest__.py
+++ purchase_order_move_menu/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "13.0.2.0.1",
+    "version": "13.0.1.0.0",
     "category": "Operations/Purchase",
     "website": "https://github.com/solvosci/slv-purchase",
     "depends": ["purchase_stock"],
