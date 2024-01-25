23ca774903c9401d0029d3ae83164aa46a0680d7
David Alonso // Solvos <david.alonso@solvos.es>
2024-01-25 17:39:14 +0100

    [ADD] purchase_order_to_approve_rpt
    Makes PO 'to approve' state labeled as 'Purchase Order' for Purchase
    order report.
    
    Template definition takes in account OCA's `purchase_order_approved`
    installed, because it difficults some `xpath` stuff.

---
+++ purchase_order_to_approve_rpt/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Purchase Order Reports - To Approve as Purchase Order",
+    "summary": """
+        Makes PO 'to approve' state labeled as 'Purchase Order'
+        for Purchase order report
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Inventory/Purchase",
+    "website": "https://github.com/solvosci/slv-purchase",
+    "depends": ["purchase"],
+    "data": ["reports/purchase_order_templates.xml"],
+}
---
+++ purchase_order_to_approve_rpt/readme/ROADMAP.rst
@@ -0,0 +1,2 @@
+Template definition takes in account OCA's `purchase_order_approved`
+installed, because it difficults some `xpath` stuff.
