98703080251ba81dbc684bc0bff0001e6fbec73f
David Alonso // Solvos <david.alonso@solvos.es>
2024-07-15 15:08:41 +0200

    [UPD] purchase_order_partner_user: make it deprecated
    `purchase_security_advanced` addon now replaces this addon
    functionality.
    
    Anyway, addon must still exist because stock_picking_mgmt_weight
    (in slv-stock repo) needs onchange method definition. When solved this
    temporary issue, this addon caould become uninstallable

--- purchase_order_partner_user/__manifest__.py
+++ purchase_order_partner_user/__manifest__.py
@@ -1,13 +1,13 @@
 # © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
 # License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
 {
-    "name": "Purchase Order Partner User",
+    "name": "DEPRECATED Purchase Order Partner User",
     "summary": """
-        Adds purchase representative to purchase order depending on the partner's Salesperson
+        DEPRECATED Adds purchase representative to purchase order depending on the partner's Salesperson
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
     'category': "Operations/Purchase",
     "website": "https://github.com/solvosci/slv-purchase",
     "depends": [
--- purchase_order_partner_user/readme/DESCRIPTION.rst
+++ purchase_order_partner_user/readme/DESCRIPTION.rst
@@ -1,1 +1,4 @@
-Adds purchase representative to purchase order depending on the partner's Salesperson
+Adds purchase representative to purchase order depending on the partner's Salesperson.
+
+NOTE This addon is deprecated, it's maintained due to backward compatibility
+with https://github.com/solvosci/slv-stock/tree/13.0/stock_picking_mgmt_weight
