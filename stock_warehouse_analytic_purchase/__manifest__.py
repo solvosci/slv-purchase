3c09722f32d4a337c299e9cc53b924618bba528f
Christian Santamaria <christian.santamaria@solvos.es>
2024-10-01 12:57:17 +0200

    [ADD] stock_warehouse_analytic_purchase

---
+++ stock_warehouse_analytic_purchase/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Stock Warehouse Analytic Purchase",
+    "summary": """
+        Adds analytical fields to lines based on the related warehouse operation type, 
+        this new definition has a higher priority than "Analytic Defaults Rules"
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "14.0.1.0.0",
+    "category": "Inventory/Purchase",
+    "website": "https://github.com/solvosci/slv-purchase",
+    "depends": ["purchase"],
+    'installable': True,
+}
