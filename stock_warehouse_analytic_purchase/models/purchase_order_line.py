87384c68a24e0f30c90faadbec3d358f47ac26a5
Christian Santamaria <christian.santamaria@solvos.es>
2024-10-01 12:57:17 +0200

    [ADD] stock_warehouse_analytic_purchase

---
+++ stock_warehouse_analytic_purchase/models/purchase_order_line.py
@@ -0,0 +1,20 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+
+from odoo import models, api
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    @api.depends('order_id.picking_type_id')
+    def _compute_account_analytic_id(self):
+        super()._compute_account_analytic_id()
+        for record in self.filtered(lambda x: x.order_id.picking_type_id.warehouse_id.account_analytic_id and not x.display_type):
+            record.account_analytic_id = record.order_id.picking_type_id.warehouse_id.account_analytic_id
+
+    @api.depends('order_id.picking_type_id')
+    def _compute_analytic_tag_ids(self):
+        super()._compute_analytic_tag_ids()
+        for record in self.filtered(lambda x: x.order_id.picking_type_id.warehouse_id.account_analytic_tag_ids and not x.display_type):
+            record.analytic_tag_ids = record.order_id.picking_type_id.warehouse_id.account_analytic_tag_ids
---
+++ stock_warehouse_analytic_purchase/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+Adds analytical fields to lines based on the related warehouse operation type, 
+this new definition has a higher priority than "Analytic Defaults Rules"
