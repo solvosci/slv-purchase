61a79d5080cc200549cfbeb48de70bcdc513a497
David Alonso // Solvos <david.alonso@solvos.es>
2024-11-30 13:30:07 +0100

    [FIX] purchase_order_update_planned_date: proper date_planned calculation
    Some fixes:
    - Original code only update main date_planned (order header,
      not in lines).
    - Date Planned was updated before PO confirmation, and we need it after
      approval (which is the moment before e.g. picking creation).

--- purchase_order_update_planned_date/models/purchase_order.py
+++ purchase_order_update_planned_date/models/purchase_order.py
@@ -1,15 +1,16 @@
 # © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
 # License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
 from odoo import models
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    def button_confirm(self):
-        super().button_confirm()
-        for order in self:
-            if order.date_order and order.date_planned:
-                dif = (order.date_approve - order.date_order).days
-                new_date_planned = order.date_planned + timedelta(days=dif)
-                order.write({'date_planned': new_date_planned})
+    def button_approve(self, force=False):
+        date_approve = datetime.now()
+        for order_line in self.order_line:
+            if order_line.order_id.date_order and order_line.date_planned:
+                dif = (date_approve - order_line.order_id.date_order).days
+                new_date_planned = order_line.date_planned + timedelta(days=dif)
+                order_line.write({'date_planned': new_date_planned})
+        super().button_approve(force=force)
