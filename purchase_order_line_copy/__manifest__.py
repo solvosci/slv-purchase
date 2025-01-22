02a6926dbf12e636e958d9aeaa691a9434f7c3f3
Ángel Rivas <angel.rivas@solvos.es>
2025-01-22 11:13:00 +0100

    [ADD] purchase_order_line_copy Adds an icon to copy purchases order lines within purchase order form

---
+++ purchase_order_line_copy/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
+{
+    "name": "Purchase Order Line Copy",
+    "summary": """
+        Adds an icon to copy purchases order lines within purchase order form
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Purchase",
+    "website": "https://github.com/solvosci/slv-purchase",
+    "depends": ["purchase"],
+    "data": [
+        "views/purchase_order_views.xml",
+    ],
+    'installable': True,
+}
