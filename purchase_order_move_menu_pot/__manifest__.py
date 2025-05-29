f0b3741271a1147f958f9a555f7c7bec6eeaac7d
Christian Santamaria <christian.santamaria@solvos.es>
2025-05-29 12:45:56 +0200

    [ADD] purchase_order_move_menu_pot Adds related order to Purchase Order Type

---
+++ purchase_order_move_menu_pot/__manifest__.py
@@ -0,0 +1,22 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Purchase Order Move Menu - Link to Purchase Order Type",
+    "summary": """
+        Adds related order to Purchase Order Type
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Operations/Purchase",
+    "website": "https://github.com/solvosci/slv-purchase",
+    "depends": [
+        "purchase_order_move_menu",
+        "purchase_order_type"
+    ],
+    "data": [
+        "views/purchase_order_type_views.xml",
+    ],
+    "installable": True,
+    "pre_init_hook": "pre_init_hook",
+}
---
+++ purchase_order_move_menu_pot/hooks.py
@@ -0,0 +1,18 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+def pre_init_hook(cr):
+    _logger.info(
+        "Pre-creating column po_menu_exclude for table stock_move"
+    )
+    cr.execute(
+        """
+        ALTER TABLE stock_move
+        ADD COLUMN IF NOT EXISTS po_menu_exclude Boolean;
+        """
+    )
