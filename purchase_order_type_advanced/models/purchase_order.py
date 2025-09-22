77be8c545b348034b90619140e04d8811ed8f378
Iria Alonso <iria.alonso@solvos.es>
2025-09-22 12:38:15 +0200

    [MIG] purchase_order_type_advanced: Migration to 17.0
    Co-Authored-By: David Alonso <david.alonso@solvos.es>

--- purchase_order_type_advanced/models/purchase_order.py
+++ purchase_order_type_advanced/models/purchase_order.py
@@ -1,7 +1,9 @@
 # © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
-# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
+from odoo import api, models
+
+import ast
 
 
 class PurchaseOrder(models.Model):
@@ -15,15 +17,29 @@ class PurchaseOrder(models.Model):
                 order.picking_type_id = order.order_type.picking_type_id
             if order.order_type.analytic_account_id:
                 order.order_line.write({
-                    "account_analytic_id": order.order_type.analytic_account_id.id,
+                    "analytic_distribution": {
+                        order.order_type.analytic_account_id.id: 100
+                    },
                 })
 
-    def action_view_invoice(self):
-        res = super(PurchaseOrder, self).action_view_invoice()
-        ctx = {}
-        if self.order_type.journal_id:
-            ctx["default_journal_id"] = self.order_type.journal_id.id
+    def _prepare_invoice(self):
+        invoice_vals = super()._prepare_invoice()
+        values = {}
         if self.order_type:
-            ctx["default_purchase_type_id"] = self.order_type.id
-        res["context"].update(ctx)
-        return res
+            values["purchase_type_id"] = self.order_type.id
+        if self.order_type.journal_id:
+            values["journal_id"] = self.order_type.journal_id.id
+
+        invoice_vals.update(values)
+
+        return invoice_vals
+
+    def action_view_invoice(self, invoices=False):
+        result = super().action_view_invoice(invoices=invoices)
+        purchase_types = self.order_type
+        if len(purchase_types) == 1:
+            result.setdefault("context", {})
+            if isinstance(result.get("context"), str):
+                result["context"] = ast.literal_eval(result["context"])
+            result["context"]["default_purchase_type_id"] = purchase_types.id
+        return result
