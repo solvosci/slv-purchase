e77c77782f76e1a07eb63c7bd0c21560302b1534
ChristianSantamaria <christiancameselle@hotmail.com>
2026-01-23 07:46:52 +0100

    [MIG] purchase_order_move_menu_pot: Migration to 17.0

--- purchase_order_move_menu_pot/hooks.py
+++ purchase_order_move_menu_pot/hooks.py
@@ -6,11 +6,11 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-def pre_init_hook(cr):
+def pre_init_hook(env):
     _logger.info(
         "Pre-creating column po_menu_exclude for table stock_move"
     )
-    cr.execute(
+    env.cr.execute(
         """
         ALTER TABLE stock_move
         ADD COLUMN IF NOT EXISTS po_menu_exclude Boolean;
