0942b1a2d1c9fbb68a96baade882d435c385c92e
cgarcia-solvos <carlos.garcia@solvos.es>
2026-02-03 11:35:46 +0100

    [ADD]purchase_order_line_desc_wo_product_code
    Avoid duplicated product codes in purchase order line descriptions

---
+++ purchase_order_line_desc_wo_product_code/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2026 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Purchase Order Line Description Without Product Code",
+    "summary": """
+        Avoid duplicated product codes in purchase order line descriptions
+    """,
+    "author": "Solvos," "Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "version": "17.0.1.0.0",
+    "category": "Purchase",
+    "website": "https://github.com/solvosci/slv-purchase",
+    "depends": [
+        "purchase",
+    ],
+    "installable": True,
+}
